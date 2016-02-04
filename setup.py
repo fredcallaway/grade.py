bdb1da292493f7a6d75e8e75899b02202cbed17d
fredcallaway <fredc@llaway.com>
2016-02-03 20:22:56 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(name='gradepy',
-      version='0.0',
+      version='0.2',
       description='Tools for automated grading of python assignments.',
+      long_description=readme(),
+      keywords='grade grading python framework student assignment',
       url='https://github.com/fredcallaway/grade.py',
       author='Fred Callaway',
       author_email='fredc@llaway.com',
